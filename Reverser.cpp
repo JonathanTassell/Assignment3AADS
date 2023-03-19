bf977440025817e39aa79632212d515129beeeb3
JonathanTassell <115473629+JonathanTassell@users.noreply.github.com>
2023-03-20 09:54:40 +1030

    Add files via upload

---
+++ Reverser.cpp
@@ -0,0 +1,32 @@
+#include "Reverser.h"
+
+std::string Reverser::reverseString(std::string characters){
+    char first = characters[0]; 
+    
+    if(characters.length() <=1)
+        return characters;
+
+    characters = characters.substr(1);
+    characters = reverseString(characters) + first;
+    return characters;
+
+
+   
+}
+
+
+int Reverser::reverseDigit(int value){
+    if(value < 0){
+        return -1; 
+    }
+    
+    int rev = 0; 
+    while(value > 0){
+        rev = rev * 10 + value % 10; 
+        value = value /10; 
+    }
+    return rev; 
+}
+
+
+
---
+++ Reverser.h
@@ -0,0 +1,13 @@
+#include <string>
+#include <iostream>
+
+#ifndef REVERSER_H
+#define REVERSER_H
+
+class Reverser{
+    public:
+        std::string reverseString(std::string characters); 
+        int reverseDigit(int value); 
+
+};
+#endif
