ebb347186d8cb017c281a540b1c60dd445cc8f42
JonathanTassell <115473629+JonathanTassell@users.noreply.github.com>
2023-03-19 20:51:47 +1030

    Add files via upload

---
+++ Truckloads.cpp
@@ -0,0 +1,14 @@
+#include "Truckloads.h"
+
+class Truckloads{
+	public:
+	int numTrucks(int numCrates, int loadSize){
+		if(numCrates <= loadSize){
+			return 1;
+        }
+		if(numCrates % 2){
+			return numTrucks(numCrates/2 +1, loadSize) + numTrucks(numCrates/2, loadSize);
+        }
+		else return 2 * numTrucks(numCrates/2, loadSize);
+	}
+}; 	
